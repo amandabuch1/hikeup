77acaf0a5da3d94c489579f9c17e729a8691f5a1
Amandabuch <amandalbuch@gmail.com>
2020-05-26 13:29:48 -0700

    Refactor the server to hash the password when a user signs up

--- models/user.js
+++ models/user.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
+
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 6;
+
 const userSchema = new mongoose.Schema({
   name: String,
   email: {type: String, required: true, lowercase: true, unique: true},
   password: String
-}, {
-  timestamps: true
+  }, {
+    timestamps: true
 });
+
 userSchema.set('toJSON', {
   transform: function(doc, ret) {
     // remove the password property when serializing doc to JSON
@@ -13,4 +19,18 @@ userSchema.set('toJSON', {
     return ret;
   }
 });
+
+userSchema.pre('save', function(next){
+  // this will be set to the current document
+  const user = this;
+  if (!user.isModified('password')) return next();
+  // password has been changed - salt and hash it
+  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+    if (err) return next(err);
+    // replace the user provided password with the hash
+    user.password = hash;
+    next();
+  });
+});
+
 module.exports = mongoose.model('User', userSchema);
