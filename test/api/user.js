165cd54674794db248336ad97d27f636a0058f4a
Amandabuch <amandalbuch@gmail.com>
2020-06-10 17:36:50 -0700

    Rename routes to test

---
+++ test/api/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/user');
+
+/*---------- Public Routes ----------*/
+router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
+
+/*---------- Protected Routes ----------*/
+module.exports = router;
